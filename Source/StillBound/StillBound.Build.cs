80b364868316ea9b4e0135362ed251c559ecb95c
05003 <sdj05003@gmail.com>
2026-01-21 16:26:07 +0900

    Fatch: Main_T_P

---
+++ Source/StillBound/StillBound.Build.cs
@@ -0,0 +1,51 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class StillBound : ModuleRules
+{
+	public StillBound(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"AIModule",
+			"StateTreeModule",
+			"GameplayStateTreeModule",
+			"UMG",
+			"Slate"
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+
+		PublicIncludePaths.AddRange(new string[] {
+			"StillBound",
+			"StillBound/Variant_Platforming",
+			"StillBound/Variant_Platforming/Animation",
+			"StillBound/Variant_Combat",
+			"StillBound/Variant_Combat/AI",
+			"StillBound/Variant_Combat/Animation",
+			"StillBound/Variant_Combat/Gameplay",
+			"StillBound/Variant_Combat/Interfaces",
+			"StillBound/Variant_Combat/UI",
+			"StillBound/Variant_SideScrolling",
+			"StillBound/Variant_SideScrolling/AI",
+			"StillBound/Variant_SideScrolling/Gameplay",
+			"StillBound/Variant_SideScrolling/Interfaces",
+			"StillBound/Variant_SideScrolling/UI"
+		});
+
+		// Uncomment if you are using Slate UI
+		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
+
+		// Uncomment if you are using online features
+		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
---
+++ Source/StillBound/StillBound.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "StillBound.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, StillBound, "StillBound" );
+
+DEFINE_LOG_CATEGORY(LogStillBound)
