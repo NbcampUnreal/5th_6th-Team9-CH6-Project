bdd6d58010284e4808c32a934f95f283ff4bca00
soloplayer-tech <nos36650@gmail.com>
2026-03-06 00:37:49 +0900

    SoundBusMix
    Master Volume / BGM Volume / SFX Volume까지 슬라이더 적용 완료 아직 BGM 뿐이라 다른 Volume 적용 후 작동 하는지 확인할 예정 및 인게임에도 옵션 창을 만들어서 그대로 옮길 예정

---
+++ Source/StillBound/Settings/SBGameUserSettings.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameUserSettings.h"
+#include "SBGameUserSettings.generated.h"
+
+class USoundControlBusMix;
+class USoundControlBus;
+
+UCLASS(Config = GameUserSettings, DefaultConfig, BlueprintType, Blueprintable)
+class STILLBOUND_API USBGameUserSettings : public UGameUserSettings
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(Config, BlueprintReadWrite, Category = "Audio", meta = (ClampMin = "0.0", ClampMax = "1.0"))
+	float MasterVolume = 1.0f;
+
+	UPROPERTY(Config, BlueprintReadWrite, Category = "Audio", meta = (ClampMin = "0.0", ClampMax = "1.0"))
+	float BGMVolume = 1.0f;
+
+	UPROPERTY(Config, BlueprintReadWrite, Category = "Audio", meta = (ClampMin = "0.0", ClampMax = "1.0"))
+	float SFXVolume = 1.0f;
+
+	// 오디오 모듈레이션 에셋들 (하드코딩 X, ini/디폴트로 지정)
+	UPROPERTY(Config, EditAnywhere, Category = "Audio|Modulation")
+	TSoftObjectPtr<USoundControlBusMix> UserBusMix;
+
+	UPROPERTY(Config, EditAnywhere, Category = "Audio|Modulation")
+	TSoftObjectPtr<USoundControlBus> MasterBus;
+
+	UPROPERTY(Config, EditAnywhere, Category = "Audio|Modulation")
+	TSoftObjectPtr<USoundControlBus> BGMBus;
+
+	UPROPERTY(Config, EditAnywhere, Category = "Audio|Modulation")
+	TSoftObjectPtr<USoundControlBus> SFXBus;
+
+	UPROPERTY(Config, EditAnywhere, Category = "Audio|Modulation", meta = (ClampMin = "0.0"))
+	float FadeTimeSec = 0.05f;
+
+	UFUNCTION(BlueprintCallable)
+	static USBGameUserSettings* Get();
+
+	UFUNCTION(BlueprintCallable, meta = (WorldContext = "WorldContextObject"))
+	void ApplyAudioSettings(UObject* WorldContextObject);
+};
