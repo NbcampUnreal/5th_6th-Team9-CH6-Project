a3670719b46072b8586785d74f02b34b70511489
도잉 <155817735+doingkite@users.noreply.github.com>
2026-03-24 01:09:28 +0900

    feat. UI
    .

--- Source/StillBound/NPC/ShopItemSlot.cpp
+++ Source/StillBound/NPC/ShopItemSlot.cpp
@@ -176,6 +176,22 @@ void UShopItemSlot::SetShopItemData(const FShopItemData& InItemData, UShopWidget
         );
         TXT_ItemPrice->SetText(PriceText);
     }
+    //재고
+    if (TXT_ItemStock)
+    {
+        if (ItemData.CurrentStock < 0)
+        {
+            TXT_ItemStock->SetText(FText::FromString( TEXT("Infi_Stock")));
+        }
+        else
+        {
+            FText StockText = FText::Format(
+                FText::FromString(TEXT("{0}G")),
+                FText::AsNumber(ItemData.CurrentStock)
+            );
+            TXT_ItemStock->SetText(StockText);
+        }
+    }
 
     // 아이콘
     if (IMG_ItemIcon && FullItemData->AssetData.Icon)
@@ -196,7 +212,7 @@ void UShopItemSlot::SetShopItemData(const FShopItemData& InItemData, UShopWidget
         if (TXT_ItemName)
         {
             FText OutOfStockText = FText::Format(
-                FText::FromString(TEXT("{0} (품절)")),
+                FText::FromString(TEXT("{0} (Out Of Stock)")),
                 FullItemData->TextData.Name
             );
             TXT_ItemName->SetText(OutOfStockText);
@@ -206,6 +222,11 @@ void UShopItemSlot::SetShopItemData(const FShopItemData& InItemData, UShopWidget
         if (TXT_ItemPrice)
         {
             TXT_ItemPrice->SetColorAndOpacity(FLinearColor(0.5f, 0.5f, 0.5f, 1.0f));
+        }
+        if (TXT_ItemStock)
+        {
+            TXT_ItemStock->SetText(FText::FromString(TEXT("Out Of Stock")));
+            TXT_ItemStock->SetColorAndOpacity(FLinearColor(0.5f, 0.5f, 0.5f, 1.0f));
         }
     }
 }
