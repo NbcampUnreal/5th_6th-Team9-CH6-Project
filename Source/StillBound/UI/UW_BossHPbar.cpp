54b0e1b88b07367377a216fa6b908d1794331014
ho0362 <ho0362@naver.com>
2026-03-24 12:09:10 +0900

    bossHPBar 데미지 딜레이 Pickup 메세지 애니메이션 오류 수정

--- Source/StillBound/UI/UW_BossHPbar.cpp
+++ Source/StillBound/UI/UW_BossHPbar.cpp
@@ -7,7 +7,17 @@ void UBossHPbar::NativeConstruct()
 {
 	Super::NativeConstruct();
 
-    SetVisibility(ESlateVisibility::Hidden);
+	SetVisibility(ESlateVisibility::Hidden);
+
+	if (HPBar)
+	{
+		HPBar->SetPercent(1.f);
+	}
+
+	if (HPBar_Delay)
+	{
+		HPBar_Delay->SetPercent(1.f);
+	}
 }
 
 void UBossHPbar::SetBossName(const FText& Name)
@@ -20,8 +30,68 @@ void UBossHPbar::SetBossName(const FText& Name)
 
 void UBossHPbar::SetHPPercent(float Percent)
 {
-    if (HPBar)
-    {
-        HPBar->SetPercent(Percent);
-    }
+	if (!HPBar || !HPBar_Delay)
+	{
+		return;
+	}
+
+	HPBar->SetPercent(Percent);
+	TargetDelayPercent = Percent;
+
+	if (UWorld* World = GetWorld())
+	{
+		World->GetTimerManager().ClearTimer(DelayStartHandle);
+		World->GetTimerManager().ClearTimer(DelayMoveHandle);
+
+		World->GetTimerManager().SetTimer(
+			DelayStartHandle,
+			this,
+			&UBossHPbar::StartDelayBarUpdate,
+			1.0f,   
+			false
+		);
+	}
+}
+
+void UBossHPbar::StartDelayBarUpdate()
+{
+	if (UWorld* World = GetWorld())
+	{
+		World->GetTimerManager().SetTimer(
+			DelayMoveHandle,
+			this,
+			&UBossHPbar::UpdateDelayHP,
+			0.02f,
+			true
+		);
+	}
+}
+
+void UBossHPbar::UpdateDelayHP()
+{
+	if (!HPBar_Delay || !HPBar)
+	{
+		return;
+	}
+
+	const float Current = HPBar_Delay->GetPercent();
+
+	const float NewPercent = FMath::FInterpTo(
+		Current,
+		TargetDelayPercent,
+		GetWorld()->GetDeltaSeconds(),
+		4.f
+	);
+
+	HPBar_Delay->SetPercent(NewPercent);
+
+	if (FMath::IsNearlyEqual(NewPercent, TargetDelayPercent, 0.005f))
+	{
+		HPBar_Delay->SetPercent(TargetDelayPercent);
+
+		if (UWorld* World = GetWorld())
+		{
+			World->GetTimerManager().ClearTimer(DelayMoveHandle);
+		}
+	}
 }
