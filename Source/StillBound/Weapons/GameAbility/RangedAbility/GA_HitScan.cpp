6dadd2e3d51509ce9ab622ef4e5fabec29c502ec
ckxlzlaoj <95674725+ckxlzlaoj@users.noreply.github.com>
2026-03-26 15:54:26 +0900

    타격 이펙트, 원거리, 투척물 버그 해결

--- Source/StillBound/Weapons/GameAbility/RangedAbility/GA_HitScan.cpp
+++ Source/StillBound/Weapons/GameAbility/RangedAbility/GA_HitScan.cpp
@@ -16,7 +16,7 @@ UGA_HitScan::UGA_HitScan()
     bFireImmediatelyIfNoMontageOrEvent = true;
 
     // µð¹ö±×
-    bDebugTrace = true;
+    bDebugTrace = false;
     DebugLifeTime = 1.0f;
     DebugLineThickness = 1.5f;
 
